877fcb559958c505c4f3884379344e77dad4e216
Abzal <43374976+Abzal12@users.noreply.github.com>
2024-07-04 21:42:55 +0600

    Stage 5/6: Working with files

--- Command.java
+++ Command.java
@@ -1,10 +1,33 @@
 package encryptdecrypt;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Command {
 
     private String userChoice = "enc";
     private int shift = 0;
     private String text = "";
+
+    private boolean hasWriter = false;
+    private String writerDocName = "";
+
+    public void writeToFile(String resultingText) {
+        File file = new File(System.getProperty("user.dir") + "\\" + writerDocName);
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(resultingText);
+        } catch (IOException e) {
+            System.out.printf("An exception occurred %s", e.getMessage());
+        }
+    }
+    public String getWriterDocName() {
+        return writerDocName;
+    }
+
+    public void setWriterDocName(String writerDocName) {
+        this.writerDocName = writerDocName;
+    }
 
     public String getUserChoice() {
         return userChoice;
@@ -12,6 +35,14 @@ public class Command {
 
     public void setUserChoice(String userChoice) {
         this.userChoice = userChoice;
+    }
+
+    public boolean isHasWriter() {
+        return hasWriter;
+    }
+
+    public void setHasWriter(boolean hasWriter) {
+        this.hasWriter = hasWriter;
     }
 
     public int getShift() {
@@ -29,4 +60,6 @@ public class Command {
     public void setText(String text) {
         this.text = text;
     }
+
+
 }
--- Main.java
+++ Main.java
@@ -1,19 +1,35 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Command command = new Command();
+        //FileWriter writer;
         for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-mode")) {
-                command.setUserChoice(args[i+1]);
-            } else if (args[i].equals("-key")) {
-                command.setShift(Integer.parseInt(args[i+1]));
-            } else if (args[i].equals("-data")) {
-                command.setText(args[i+1]);
+            switch (args[i]) {
+                case "-mode" -> command.setUserChoice(args[i + 1]);
+                case "-key" -> command.setShift(Integer.parseInt(args[i + 1]));
+                case "-data" -> command.setText(args[i + 1]);
+                case "-in" -> {
+                    try {
+                        String docText = new String
+                                (Files.readAllBytes(Paths.get(System.getProperty("user.dir") + "\\" + args[i + 1])));
+                        if (command.getText().isEmpty()) {
+                            command.setText(docText);
+                        }
+                    } catch (IOException e) {
+                        System.out.println("Error: no such file");
+                    }
+                }
+                case "-out" -> {
+                    command.setHasWriter(true);
+                    command.setWriterDocName(args[i+1]);
+                }
             }
         }
 
@@ -24,10 +40,18 @@ public class Main {
 
         if (targetOperation.equals("enc")) {
             String encryptedText = Key.encryptUsingAscii(inputText, shift);
-            System.out.println(encryptedText);
+            if (command.isHasWriter()) {
+                command.writeToFile(encryptedText);
+            } else {
+                System.out.println(encryptedText);
+            }
         } else if (targetOperation.equals("dec")) {
             String decryptedText = Key.decryptUsingAscii(inputText, shift);
-            System.out.println(decryptedText);
+            if (command.isHasWriter()) {
+                command.writeToFile(decryptedText);
+            } else {
+                System.out.println(decryptedText);
+            }
         }
     }
 }
